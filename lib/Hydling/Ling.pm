149f3a8ee96442feaecef91aa782251a57822bf1
Matt S Trout <mst@shadowcat.co.uk>
2021-09-07 00:55:19 +0000

    better ling sketch

--- lib/Hydling/Ling.pm
+++ lib/Hydling/Ling.pm
@@ -2,7 +2,7 @@ package Hydling::Ling;
 
 use Hydling::ControlPort;
 use Mojo::IRC;
-use Mojo::Base -base, -signatures, -async_await;
+use Hydling::Class;
 
 has 'control_port_path';
 
@@ -10,59 +10,46 @@ has 'control_port';
 
 has 'irc';
 
-sub control_port_config ($self) {
-  return (
-    socket_path => $self->control_port_path,
-    dispatch_call => $self->curry::weak::dispatch_call,
-    dispatch_listen => $self->curry::weak::dispatch_listen,
-  );
-}
-
 sub start ($self) {
   $self->control_port(
     Hydling::ControlPort->new(
-      $self->control_port_config
-    )->start
+    socket_path => $self->control_port_path,
+    )
+    ->tap($self->curry::control_port_setup)
+    ->start
   )->irc(Mojo::IRC->new->nick('hydling'));
 }
 
-sub dispatch_call ($self, $to, @args) {
-  if ($to =~ /^(?:dis)?connect/) {
-    return $self->irc->${\"${to}_p"};
+sub control_port_setup ($self, $control_port) {
+  my $h = $control_port->handlers;
+  foreach my $name (qw(connect disconnect)) {
+    $h->call($name => sub { $self->irc->${\"${name}_p"} });
   }
-  if ($to eq 'write') {
+  $h->call(write => sub ($r, @args) {
     my @send = map +(ref($_) ? $self->irc->ctcp(@$_) : $_), @args;
-    return $self->irc->write_p(@send);
-  }
-  state %is_setter = (
-    map +($_ => 1),
-      qw(connect_timeout local_address name nick pass real_host server)
-  );
-  if ($is_setter{$to}) {
-    $self->$to($args[0]);
-    return 'done';
-  }
-  return 'nope';
-}
-
-sub dispatch_listen ($self, $listener, $to, @) {
-  state %is_event = (
-    map +($_ => 1),
-      qw(connecting connected disconnecting disconnected message status)
-  );
-  if (
-    $is_event{$to}
-    or $to =~ m/^(?:irc|ctcp)_/
+    $self->irc->write_p(@send);
+  });
+  foreach my $accessor (
+    qw(connect_timeout local_address name nick pass real_host server)
   ) {
-    my $notify = $self->curry::notify_listener($listener);
-    $self->irc->on($to => $notify);
-    return $self->irc->curry::unsubscribe($to => $notify);
+    $h->call($accessor => sub ($r, $value = undef) {
+      $self->$accessor($value//());
+      $r->done($self->$accessor);
+    });
   }
-  return 'nope';
-}
-
-sub notify_listener ($self, $listener, $, $event, @payload) {
-  $self->control_port->notify($listener, $event, @payload);
+  $h->listen(qr/^(?:irc|ctcp)_/, sub ($r) {
+    my $name = $r->done->listen;
+    my $on = sub ($, @args) { $r->notify(@args) };
+    my $cb = $self->irc->on($name => $r->curry::notify);
+    $r->on_unlisten(sub { $self->irc->unsubcribe($name => $cb) });
+  });
+  $h->watch(status => sub ($r) {
+    my $cb = $self->on(status => sub ($, $value) {
+      $r->send($value);
+    });
+    $r->on_unwatch(sub { $self->unsubscribe(status => $cb) });
+    $r->done->send($self->status);
+  });
 }
 
 1;
