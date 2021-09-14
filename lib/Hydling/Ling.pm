e0fb7c3a9631d480306c3aa7cf9ac9e6ad1077e8
Matt S Trout <mst@shadowcat.co.uk>
2021-09-14 01:14:53 +0000

    Ling cleanup

--- lib/Hydling/Ling.pm
+++ lib/Hydling/Ling.pm
@@ -1,58 +1,77 @@
 package Hydling::Ling;
 
-use Hydling::ControlPort;
 use Mojo::IRC;
+use Mojo::StupidRPC;
 use Hydling::Base;
 
-has 'control_port_path';
-
-has 'control_port';
+has 'control_port_config';
 
 has 'irc';
 
-sub on { shift->irc->on(@_) }
-sub unsubscribe { shift->irc->unsubscribe(@_) }
-
-sub start ($self) {
-  $self->control_port(
-    Hydling::ControlPort->server(
-      path => $self->control_port_path,
-    )
-    ->tap($self->curry::control_port_setup)
-    ->start
-  )->irc(Mojo::IRC->new->nick('hydling'));
-}
-
-sub control_port_setup ($self, $control_port) {
-  my $h = $control_port->handlers;
+has control_port_handlers => sub ($self) {
+  Scalar::Util::weaken($self);
+  my $h = Mojo::StupidRPC->handler_set;
   foreach my $name (qw(connect disconnect)) {
-    $h->call($name => sub { $self->irc->${\"${name}_p"} });
+    my $method = "${name}_p}";
+    $h->call($name => sub ($r, @) {
+      $self->irc
+           ->$method
+           ->then(
+               sub { $r->done },
+               sub ($self, $err) { $r->fail($err) },
+             );
+    });
   }
-  $h->call(write => sub ($r, @args) {
-    my @send = map +(ref($_) ? $self->irc->ctcp(@$_) : $_), @args;
-    $self->irc->write_p(@send);
+  $h->call(send => sub ($r, @args) {
+    my $last = $args[-1];
+    my @send = map {
+      if (ref($_)) {
+        $self->irc->ctcp(@$_)
+      } elsif ($_ !~ / /) {
+        $_
+      } elsif ($_ eq $last) {
+        ":$_"
+      } else {
+        return $r->fail("Invalid input for send")
+      }
+    } @args;
+    $self->irc
+         ->write_p(@send)
+         ->then(
+             sub { $r->done },
+             sub ($self, $err) { $r->fail($err) },
+           );
   });
   foreach my $accessor (
     qw(connect_timeout local_address name nick pass real_host server)
   ) {
     $h->call($accessor => sub ($r, @value) {
       $self->$accessor($value[0]) if @value;
-      return $self->$accessor;
+      $r->done($self->$accessor);
     });
   }
-  $h->listen(qr/^(?:irc|ctcp)_/, sub ($r) {
-    my $name = $r->name;
-    my $cb = $self->on($name => sub ($, @args) { $r->notify(@args) });
-    $r->on_cancel($self->curry::unsubscribe($name => $cb));
-    return;
-  });
-  $h->listen(status => sub ($r) {
-    my $cb = $self->on(status => sub ($, $value) {
-      $r->notify($value);
+  foreach my $thing (qw(status message)) {
+    $h->listen(message => sub ($r) {
+      Scalar::Util::weaken($r);
+      my $name = $r->name;
+      my $cb = $self->on($name => sub ($, @args) { $r->notify(@args) });
+      $r->once(cancel => $self->curry::unsubscribe($name => $cb));
+      $r->done($thing eq 'status' ? $self->status : ());
+      return;
     });
-    $r->on_cancel($self->curry::unsubscribe(status => $cb));
-    return $self->status;
-  });
+  }
+  return $h;
+};
+
+sub on { shift->irc->on(@_) }
+sub unsubscribe { shift->irc->unsubscribe(@_) }
+
+sub start ($self) {
+  Mojo::StupidRPC->server(
+    $self->control_port_config,
+    $self->control_port_handlers,
+  );
+  $self->irc(Mojo::IRC->new->nick('hydling'));
 }
 
 1;
