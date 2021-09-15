10a2ac3d750752217f428059347cf17885c697b3
Matt S Trout <mst@shadowcat.co.uk>
2021-09-15 04:18:31 +0000

    better Base code

--- lib/Hydling/Base.pm
+++ lib/Hydling/Base.pm
@@ -2,17 +2,30 @@ package Hydling::Base;
 
 use curry;
 use Import::Into;
+use base qw(Exporter);
 use Mojo::Base -strict, -signatures;
+
+our @EXPORT = qw(_load_my);
 
 sub import ($me, $base = '-base') {
   Feature::Compat::Try->import::into(1);
   Carp->import::into(1, 'croak');
+  unless ($base eq '-role') {
+    Role::Tiny::With->import::into(1);
+    Class::Method::Modifiers->import::into(1);
+  }
+  $me->export_to_level(1);
   Safe::Isa->import::into(1);
-  Role::Tiny::With->import::into(1);
   Mojo::Base->import::into(1, $base, -signatures, -async_await);
   warnings->import::into(1, FATAL => 'uninitialized');
   warnings->unimport::out_of(1, 'once');
   experimental->import::into(1, qw(declared_refs refaliasing));
+}
+
+sub _load_my ($name) {
+  my $package = __PACKAGE__ =~ s/Base$/${name}/r;
+  require join('/', split '::', $package).'.pm';
+  return $package;
 }
 
 1;
